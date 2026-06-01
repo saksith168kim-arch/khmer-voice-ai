ec8154bb69a088f6a364835c61248e1d4aaa76ab
kimsaksith <saksithlove@gmail.com>
2026-06-01 15:31:40 +0700

    fix: stripe api version

--- src/lib/stripe/index.ts
+++ src/lib/stripe/index.ts
@@ -3,7 +3,7 @@ import { prisma } from '@/lib/db/prisma'
 import { PLAN_LIMITS } from '@/types'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-02-24.acacia',
   typescript: true,
 })
 
